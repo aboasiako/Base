8a709cccaff86a1c6a6cef694f36515c3d0b7370
sirFred <119733386+aboasiako@users.noreply.github.com>
2025-01-11 19:33:26 +0000

    Delete Sol_Wallet directory

--- Sol_Wallet/wallet/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from solana.rpc.api import Client
-from solana.keypair import Keypair
-from solana.transaction import Transaction
-from solana.system_program import TransferParams, transfer
-import base58
